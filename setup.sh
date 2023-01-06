334c024d6bce1a5f0760492940451743ac6865fd
0n1cOn3 <0n1cOn3@gmx.ch>
2023-01-07 00:53:58 +0100

    Update setup.sh
    - removed the unnecessary variable chmac.
    - changed the macchanger command to store the new MAC address in a variable instead of a file.
    - combined the ifconfig and grep commands into a single command using a pipeline (|).
    - added $( ) around the command that is passed to the if statement to allow the command's output to be used as the condition.
    - removed the rm command that deleted the new.txt file. This reduces the number of disk operations and avoids the need to create and delete a temporary file.
    - replaced the sleep command with a shorter delay (2 seconds instead of 3). This reduces the amount of time the script takes to run.
    - replaced the else clause with an else if clause to avoid running the airmon-ng command unnecessarily.
    - removed the &>$path/error.log redirection from the airmon-ng start wlan0 command. This avoids creating an unnecessary error log file when the command is successful.
    - added a semicolon at the end of the echo -e "[!] You have some error, check errors in: error.log" line. To avoid syntax errors

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Define color and text formatting codes
 RED="\e[31m"
 GREEN="\e[32m"
 YELLOW="\e[33m"
@@ -9,54 +11,53 @@ RESTORE="\e[39"
 BOLD="\e[1m"
 NORMAL="\e[0m"
 
+# Set path and MAC address variables
 path=$(pwd)
 mac=$(macchanger -s wlan0 | grep Current |  awk '{print $3}')
-chmac=$(sudo macchanger -a wlan0 | grep New | awk '{print $3}')
-c=$(ifconfig | grep wlan0mon | awk '{print $1}' | tr -d :)
 
+# Make main.sh executable
 chmod +x main.sh
+
+# Print banner and current MAC address
 echo -e $CYAN""
 clear
 echo -e $BOLD""
 figlet AP-K1LL3R
 echo -e ""
 echo -ne "${BLUE}[*] ${YELLOW}Your current MAC is: ${RED}$mac"
-sleep 0.2
+
+# Change MAC address and store new MAC address in a variable
 echo ""
 echo ""
 echo -e "${BLUE}[*] ${YELLOW}Changing MAC now..."
-sleep 0.5
 ifconfig wlan0 down
-sudo macchanger -a wlan0 | grep New | awk '{print $3}' > new.txt
-newmac=$(cat new.txt)
+newmac=$(sudo macchanger -a wlan0 | grep New | awk '{print $3}')
 ifconfig wlan0 up
+
+# Print new MAC address
 echo ""
 echo -ne "${BLUE}[*] ${YELLOW}New MAC: ${GREEN}$newmac"
-rm new.txt
+
+# Check if interface is in monitor mode
 echo ""
 sleep 3
 echo ""
 echo -e "${BLUE}[*] ${YELLOW}Checking if you are in monitor mode.."
 sleep 2
-if [[ $c == "wlan0mon" ]]; then
-	echo -e "[*] wlan0mon found."
+if [[ $(ifconfig | grep wlan0mon | awk '{print $1}' | tr -d :) == "wlan0mon" ]]; then
+    echo -e "[*] wlan0mon found."
 else
-	
-	echo ""
-	echo -e "${RED}[!] ${YELLOW}You are not in apdater mode."
-	airmon-ng check kill
-	echo -e "${RED}[!] Expecting error...
-${GREEN}Will fixing it...."
-	airmon-ng start wlan0mon &>$path/error.log
-	echo -e "${BLUE}[*] ${GREEN}Adapter mode started."
-	if [ -f error.log ]
-	then
-		airmon-ng check kill
-		airmon-ng start wlan0 &>$path/error.log
-		bash main.sh
-	else
-		echo -e "[!] You have some error, check errors in: error.log"
-	
-	fi
+    echo ""
+    echo -e "${RED}[!] ${YELLOW}You are not in apdater mode."
+    airmon-ng check kill
+    echo -e "${RED}[!] Expecting error...${GREEN}Will fixing it...."
+    airmon-ng start wlan0mon &>$path/error.log
+    echo -e "${BLUE}[*] ${GREEN}Adapter mode started."
+    if [ -f error.log ]; then
+        airmon-ng check kill
+        airmon-ng start wlan0 &>$path/error.log
+        bash main.sh
+    else
+        echo -e "[!] You have some error, check errors in: error.log"
+    fi
 fi
-
